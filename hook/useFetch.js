0e8f629405e84b748b55b431a3d5945e77c6607b
DemetreShonia <shonka.demeloper@gmail.com>
2023-08-02 19:46:19 +0400

    Modify PopularJobCard

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { RAPID_API_KEY } from "@env";
-const rapidApiKey = RAPID_API_KEY;
+// import { RAPID_API_KEY } from "@env";
+// const rapidApiKey = RAPID_API_KEY;
 
 const useFetch = (endpoint, query) => {
   const [data, setData] = useState([]);
@@ -12,7 +12,7 @@ const useFetch = (endpoint, query) => {
     method: "GET",
     url: `https://jsearch.p.rapidapi.com/${endpoint}`,
     headers: {
-      "X-RapidAPI-Key": rapidApiKey,
+      "X-RapidAPI-Key": "97c7dfa6f2msh36ebf5eb04618dep1ed103jsn9408dab1d7a7",
       "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
     },
     params: { ...query },
@@ -22,7 +22,6 @@ const useFetch = (endpoint, query) => {
     setIsLoading(true);
     try {
       const response = await axios.request(options);
-
       setData(response.data.data);
       setIsLoading(false);
     } catch (error) {
@@ -44,3 +43,5 @@ const useFetch = (endpoint, query) => {
 
   return { data, isLoading, error, refetch };
 };
+
+export default useFetch;
